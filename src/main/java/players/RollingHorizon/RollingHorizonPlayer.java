b62167ca6603384bbe91280379fdc423ce918255
ShiroLatte <65367333+ShiroLatte@users.noreply.github.com>
2022-05-25 23:33:21 -0400

    Se suma framework
    Se agrega el framework mencionado en el informe para trabajar con el algoritmo evolutivo

--- src/main/java/players/RollingHorizon/RollingHorizonPlayer.java
+++ src/main/java/players/RollingHorizon/RollingHorizonPlayer.java
@@ -1,4 +1,4 @@
-package players.simple;
+package players.RollingHorizon;
 
 import core.AbstractForwardModel;
 import core.AbstractGameState;
@@ -8,35 +8,26 @@ import core.actions.AbstractAction;
 import java.util.List;
 import java.util.Random;
 
-public class RandomPlayer extends AbstractPlayer {
+public class RollingHorizonPlayer extends AbstractPlayer {
 
-    /**
-     * Random generator for this agent.
-     */
-    private final Random rnd;
-
-    public RandomPlayer(Random rnd) {
-        this.rnd = rnd;
-    }
-
-    public RandomPlayer()
+    private final Random rhea;
+    
+    //contructor de jugador
+    public RollingHorizonPlayer()
     {
-        this(new Random());
+        this(new RollingHorizonPlayer());
     }
 
+    //metodo abstracto de conseguir accion, cambiar en un futuro
     @Override
     public AbstractAction getAction(AbstractGameState observation, List<AbstractAction> actions) {
-        int randomAction = rnd.nextInt(actions.size());
+        int randomAction = rhea.nextInt(actions.size()); //elige una accion random con el metodo nextInt de Java util random
         return actions.get(randomAction);
     }
 
     @Override
-    public String toString() {
-        return "Random";
-    }
-
-    @Override
-    public RandomPlayer copy() {
-        return new RandomPlayer(new Random(rnd.nextInt()));
+    public AbstractPlayer copy() {
+        // TODO Auto-generated method stub
+        return null;
     }
 }
